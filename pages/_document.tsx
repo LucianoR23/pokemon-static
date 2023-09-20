6bbe2408e1553d89ab2293066420becd00d193a9
LucianoR23 <231097lr@gmail.com>
2023-09-20 09:33:09 -0300

    Modificacion material icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"/>
